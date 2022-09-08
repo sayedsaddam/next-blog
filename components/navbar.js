76f7c88b5781b0fca9639dbd39daa6adf5098733
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2022-09-08 23:01:00 +0500

    updated 5 files, added image on to the blog homepage, created a navbar component inside the components folder, added styles to pages and headings, paragarphs and other tags.:

---
+++ components/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from 'next/link';
+import styles from '../styles/Home.module.css'
+
+const Navbar = () => {
+  return (
+   <nav className={styles.mainnav}>
+      <ul>
+         <Link href="/" passHref><li>Home</li></Link>
+         <Link href="/about" passHref><li>About</li></Link>
+         <Link href="/blog" passHref><li>Blog</li></Link>
+         <Link href="/contact" passHref><li>Contact</li></Link>
+      </ul>
+   </nav>
+  )
+}
+
+export default Navbar;
