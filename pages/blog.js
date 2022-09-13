6b302497be0bf691195db66ab09cab09062d988b
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2022-09-13 11:58:43 +0500

    updated 7 files including the newly created ones, implemented the infinite scroll in react using the built-in react library, displayed 2 records initially and by scrolling fetching more posts/records on to the page.

--- pages/blog.js
+++ pages/blog.js
@@ -2,39 +2,47 @@ import React, { useEffect, useState } from 'react'
 import styles from '../styles/Blog.module.css'
 import Link from 'next/link'
 import * as fs from 'fs'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 // Step 1: Collect all the files from the blogdata directory
 // Step 2: Iterate through them and Display them
 
-const Blog = () => {
-  const [blogs, setBlogs] = useState([])
-  useEffect(() => {
-    // console.log('useEffect is running...')
-    fetch('http://localhost:3000/api/blogs').then((a) => {
-      return a.json()
-    })
-    .then((parsed) => {
-      // console.log(parsed)
-      setBlogs(parsed)
-    })
-  }, [])
+const Blog = (props) => {
+  const [blogs, setBlogs] = useState(props.allBlogs)
+  const [count, setCount] = useState(2)
+  const fetchData = async () => {
+    let d = await fetch(`http://localhost:3000/api/blogs/?count=${count + 2}`)
+    setCount(count + 2)
+    let data = await d.json()
+    setBlogs(data)
+  }
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        {blogs.map((blogitem) => {
-          return (
-            <div key={blogitem.slug}>
-              <Link href={`/posts/${blogitem.slug}`}>
-                <h3 className={styles.blogItemh3}>{blogitem.title}</h3>
-              </Link>
-              <small>By: {blogitem.author} on {Date()}</small>
-              <p className={styles.blogItemp}>{blogitem.metadesc.substr(0, 140)}...</p>
-              <Link href={`/posts/${blogitem.slug}`}>
-                <button className={styles.btn}>Read more</button>
-              </Link>
-            </div>
-          )
-        })}
+        <InfiniteScroll
+        dataLength={blogs.length}
+        next={fetchData}
+        hasMore={props.allCount !== blogs.length}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <strong>Yay! You're all set...</strong>
+          </p>
+        }>
+          {blogs.map((blogitem) => {
+            return (
+              <div key={blogitem.slug}>
+                <Link href={`/posts/${blogitem.slug}`}>
+                  <h3 className={styles.blogItemh3}>{blogitem.title}</h3>
+                </Link>
+                <p className={styles.blogItemp}>{blogitem.metadesc.substr(0, 140)}...</p>
+                <Link href={`/blogpost/${blogitem.slug}`}>
+                  <button className={styles.btn}>Read More</button>
+                </Link>
+              </div>
+            )
+          })}
+        </InfiniteScroll>
       </main>
     </div>
   )
@@ -52,16 +60,17 @@ const Blog = () => {
 // getStaticProps
 export async function getStaticProps(context){
   let data = await fs.promises.readdir('blogdata')
+  let allCount = data.length
   let myfile;
   let allBlogs = []
-  for(let index = 0; index < data.length; index++){
+  for(let index = 0; index < 2; index++){
     const item = data[index]
     // console.log(item)
     myfile = await fs.promises.readFile((`blogdata/`+item), 'utf-8')
     allBlogs.push(JSON.parse(myfile))
   }
   return {
-    props: {allBlogs}
+    props: {allBlogs, allCount}
   }
 }
 
