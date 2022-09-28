2b9b42d97a668f3cfab0d182bb0bd693bc020a95
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2022-09-28 14:51:22 +0500

    updated 3 files, removed the about.module.css, installed tailwindcss and put its classes in about.js

--- pages/about.js
+++ pages/about.js
@@ -1,27 +1,46 @@
 import React from 'react'
-import styles from '../styles/About.module.css'
 
 export const About = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.center}>About NextJS Blog</h1>
-      <h2>Introduction</h2>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus expedita eos consequuntur ipsum cupiditate aliquam? Perspiciatis earum corrupti voluptatem reprehenderit, odit quos animi inventore totam quasi facere nesciunt praesentium ex.</p>
-      <h2>Services Offered</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui exercitationem, porro doloremque itaque veritatis doloribus repellat necessitatibus dignissimos fuga vitae aut laboriosam. Expedita adipisci, quasi qui ab earum labore sit.</p>
+    <div className='container mx-auto pt-10'>
+      <h1 className="text-center font-extrabold text-4xl pb-10">About this Blog</h1>
+      <h2 className="text-2xl font-semibold pb-4">Introduction</h2>
+      <p className='font-light'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus expedita eos consequuntur ipsum cupiditate aliquam? Perspiciatis earum corrupti voluptatem reprehenderit, odit quos animi inventore totam quasi facere nesciunt praesentium ex.</p>
+
+      <h2 className='text-2xl font-semibold py-4'>Services Offered</h2>
+      <p className='font-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui exercitationem, porro doloremque itaque veritatis doloribus repellat necessitatibus dignissimos fuga vitae aut laboriosam. Expedita adipisci, quasi qui ab earum labore sit.</p>
       <p>We offer the following services</p>
-      <ul>
-      <li>Service 1</li>
-        <li>Service 1</li>
-        <li>Service 2</li>
-        <li>Service 3</li>
-        <li>Service 4</li>
-        <li>Service 5</li>
-        <li>Service 6</li>
-        <li>Service 7</li>
-      </ul>
-      <h2>Contact Us</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur facere blanditiis deserunt a, provident assumenda quasi ad debitis, reiciendis rem ab vero voluptas velit porro reprehenderit fuga commodi voluptatum! Esse!</p>
+      <div className='grid grid-cols-3 gap-4 text-center pt-4 pb-4'>
+        <div className='bg-white p-4 rounded text-black'>
+          <h2 className='font-semibold'>Web Development</h2>
+        </div>
+        <div className='bg-white p-4 rounded text-black'>
+          <h2 className='font-semibold'>Web Applications</h2>
+        </div>
+        <div className='bg-white p-4 rounded text-black'>
+          <h2 className='font-semibold'>Mobile Applications</h2>
+        </div>
+        <div className='bg-white p-4 rounded text-black'>
+          <h2 className='font-semibold'>Databases</h2>
+        </div>
+        <div className='bg-white p-4 rounded text-black'>
+          <h2 className='font-semibold'>Network</h2>
+        </div>
+        <div className='bg-white p-4 rounded text-black'>
+          <h2 className='font-semibold'>Algorithms</h2>
+        </div>
+        <div className='bg-white p-4 rounded text-black'>
+          <h2 className='font-semibold'>Data Structures</h2>
+        </div>
+        <div className='bg-white p-4 rounded text-black'>
+          <h2 className='font-semibold'>UI/UX</h2>
+        </div>
+        <div className='bg-white p-4 rounded text-black'>
+          <h2 className='font-semibold'>Frontend/Backend</h2>
+        </div>
+      </div>
+      <h2 className='font-semibold text-2xl py-4'>Contact Us</h2>
+      <p className='font-light py-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur facere blanditiis deserunt a, provident assumenda quasi ad debitis, reiciendis rem ab vero voluptas velit porro reprehenderit fuga commodi voluptatum! Esse!</p>
     </div>
   )
 }
--- pages/others.js
+++ pages/others.js
@@ -3,9 +3,9 @@ import Head from "next/head"
 
 export default function Others() {
    return (
-      <div className="my-10 container mx-auto">
-         <h1 className="font-extrabold text-amber-400 text-4xl">Tailwind Installed</h1>
-         <p className="text-amber-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit placeat doloremque fugit dignissimos excepturi quidem cupiditate nam consectetur sequi! Consectetur repudiandae quidem, soluta dolorem maiores id totam cupiditate nemo voluptatum.</p>
+      <div className="my-10 container mx-auto border-spacing-5 bg-white text-black m-6 p-6 rounded-md selection:bg-red-700 selection:text-white">
+         <h1 className="font-extrabold text-4xl mb-5">Tailwind Installed</h1>
+         <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit placeat doloremque fugit dignissimos excepturi quidem cupiditate nam consectetur sequi! Consectetur repudiandae quidem, soluta dolorem maiores id totam cupiditate nemo voluptatum.</p>
       </div>
    )
 }
