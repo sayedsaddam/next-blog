d62aa22eb81bebdeb42dac8608048a904f69eb9a
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2022-09-28 17:44:38 +0500

    updated others.js, added tailwind classes in it.

--- pages/others.js
+++ pages/others.js
@@ -4,7 +4,7 @@ import Head from "next/head"
 export default function Others() {
    return (
       <div className="container mx-auto my-10">
-         <div className="border-spacing-5 bg-white text-black m-6 p-6 rounded-md selection:bg-red-700 selection:text-white">
+         <div className="border-spacing-5 bg-white text-black m-6 p-6 rounded-md selection:bg-red-400 selection:text-white">
             <h1 className="font-extrabold text-4xl mb-5">Tailwind Installed</h1>
             <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit placeat doloremque fugit dignissimos excepturi quidem cupiditate nam consectetur sequi! Consectetur repudiandae quidem, soluta dolorem maiores id totam cupiditate nemo voluptatum.</p>
          </div>
