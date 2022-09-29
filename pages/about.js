22963c76f47b5b42e0987130c6684801620be50f
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2022-09-29 14:41:33 +0500

    updated 4 files including index.js, about.js, globals.css.

--- pages/about.js
+++ pages/about.js
@@ -2,46 +2,216 @@ import React from 'react'
 
 export const About = () => {
   return (
-    <div className='container mx-auto pt-10'>
-      <h1 className="text-center font-extrabold text-4xl pb-10">About this Blog</h1>
-      <h2 className="text-2xl font-semibold pb-4">Introduction</h2>
-      <p className='font-light'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus expedita eos consequuntur ipsum cupiditate aliquam? Perspiciatis earum corrupti voluptatem reprehenderit, odit quos animi inventore totam quasi facere nesciunt praesentium ex.</p>
+    <>
+      <div className="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4">
+        <div className="flex flex-col lg:flex-row justify-between gap-8">
+          <div className="w-full lg:w-5/12 flex flex-col justify-center">
+            <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">About Us</h1>
+            <p className="font-normal text-base leading-6 text-gray-600 ">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed; which is apparent from</p>
+          </div>
+          <div className="w-full lg:w-8/12 ">
+            <img className="w-full h-full" src="https://i.ibb.co/FhgPJt8/Rectangle-116.png" alt="A group of People" />
+          </div>
+        </div>
 
-      <h2 className='text-2xl font-semibold py-4'>Services Offered</h2>
-      <p className='font-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui exercitationem, porro doloremque itaque veritatis doloribus repellat necessitatibus dignissimos fuga vitae aut laboriosam. Expedita adipisci, quasi qui ab earum labore sit.</p>
-      <p>We offer the following services</p>
-      <div className='grid grid-cols-3 gap-4 text-center pt-4 pb-4'>
-        <div className='bg-white p-4 rounded text-black'>
-          <h2 className='font-semibold'>Web Development</h2>
-        </div>
-        <div className='bg-white p-4 rounded text-black'>
-          <h2 className='font-semibold'>Web Applications</h2>
-        </div>
-        <div className='bg-white p-4 rounded text-black'>
-          <h2 className='font-semibold'>Mobile Applications</h2>
-        </div>
-        <div className='bg-white p-4 rounded text-black'>
-          <h2 className='font-semibold'>Databases</h2>
-        </div>
-        <div className='bg-white p-4 rounded text-black'>
-          <h2 className='font-semibold'>Network</h2>
-        </div>
-        <div className='bg-white p-4 rounded text-black'>
-          <h2 className='font-semibold'>Algorithms</h2>
-        </div>
-        <div className='bg-white p-4 rounded text-black'>
-          <h2 className='font-semibold'>Data Structures</h2>
-        </div>
-        <div className='bg-white p-4 rounded text-black'>
-          <h2 className='font-semibold'>UI/UX</h2>
-        </div>
-        <div className='bg-white p-4 rounded text-black'>
-          <h2 className='font-semibold'>Frontend/Backend</h2>
+        <div className="flex lg:flex-row flex-col justify-between gap-8 pt-12">
+          <div className="w-full lg:w-5/12 flex flex-col justify-center">
+            <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">Our Story</h1>
+            <p className="font-normal text-base leading-6 text-gray-600 ">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed; which is apparent from</p>
+          </div>
+          <div className="w-full lg:w-8/12 lg:pt-8">
+            <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:gap-4 shadow-lg rounded-md">
+              <div className="p-4 pb-6 flex justify-center flex-col items-center">
+                <img className="md:block hidden" src="https://i.ibb.co/FYTKDG6/Rectangle-118-2.png" alt="Alexa featured Img" />
+                <img className="md:hidden block" src="https://i.ibb.co/zHjXqg4/Rectangle-118.png" alt="Alexa featured Img" />
+                <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Alexa</p>
+              </div>
+              <div className="p-4 pb-6 flex justify-center flex-col items-center">
+                <img className="md:block hidden" src="https://i.ibb.co/fGmxhVy/Rectangle-119.png" alt="Olivia featured Img" />
+                <img className="md:hidden block" src="https://i.ibb.co/NrWKJ1M/Rectangle-119.png" alt="Olivia featured Img" />
+                <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Olivia</p>
+              </div>
+              <div className="p-4 pb-6 flex justify-center flex-col items-center">
+                <img className="md:block hidden" src="https://i.ibb.co/Pc6XVVC/Rectangle-120.png" alt="Liam featued Img" />
+                <img className="md:hidden block" src="https://i.ibb.co/C5MMBcs/Rectangle-120.png" alt="Liam featued Img" />
+                <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Liam</p>
+              </div>
+              <div className="p-4 pb-6 flex justify-center flex-col items-center">
+                <img className="md:block hidden" src="https://i.ibb.co/7nSJPXQ/Rectangle-121.png" alt="Elijah featured img" />
+                <img className="md:hidden block" src="https://i.ibb.co/ThZBWxH/Rectangle-121.png" alt="Elijah featured img" />
+                <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Elijah</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <h2 className='font-semibold text-2xl py-4'>Contact Us</h2>
-      <p className='font-light py-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur facere blanditiis deserunt a, provident assumenda quasi ad debitis, reiciendis rem ab vero voluptas velit porro reprehenderit fuga commodi voluptatum! Esse!</p>
-    </div>
+
+      <div className="md:mx-auto md:container px-4">
+        <div className="pt-4 md:pt-10">
+          <div className="container mx-auto">
+            <div className="flex flex-wrap items-center pb-12">
+              <div className="md:w-1/2 lg:w-2/3 w-full xl:pr-20 md:pr-6">
+                <div className="py-2 text-color">
+                  <h1 className="text-2xl lg:text-6xl md:leading-snug text-black tracking-tighter f-f-l font-black">Boost adoption of product and services</h1>
+                  <h2 className="text-lg lg:text-3xl lg:leading-7 text-black md:leading-10 f-f-r py-4 md:py-8">Here at Globex we take special care of what your organization needs instead of selling you a mass market tool that takes a one size fits all approach. I personally review each and every client business and oversee the team that tailores a solution</h2>
+                  <div className="flex items-center cursor-pointer pb-4 md:pb-0">
+                    <h3 className="f-f-r text-lg lg:text-2xl font-semibold underline text-indigo-700">Lets Get Started</h3>
+                    <div className="pl-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none">
+                          <path d="M13.1719 12L8.22192 7.04999L9.63592 5.63599L15.9999 12L9.63592 18.364L8.22192 16.95L13.1719 12Z" fill="#D53F8C" />
+                        </svg>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="lg:w-1/3 md:w-1/2 w-full relative h-96 flex items-end justify-center">
+                <img className="absolute w-full h-full inset-0 object-cover object-center rounded-md" src="https://cdn.tuk.dev/assets/templates/prodify/ProductAdoption.png" alt />
+                <div className="relative z-10 bg-white rounded shadow p-6 w-10/12 -mb-20">
+                  <div className="flex items-center justify-between w-full sm:w-full mb-8">
+                    <div className="flex items-center">
+                      <div className="p-4 bg-yellow-200 rounded-md">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-discount" width={32} height={32} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                          <path stroke="none" d="M0 0h24v24H0z" />
+                          <line x1={9} y1={15} x2={15} y2={9} />
+                          <circle cx="9.5" cy="9.5" r=".5" />
+                          <circle cx="14.5" cy="14.5" r=".5" />
+                          <path d="M5 7.2a2.2 2.2 0 0 1 2.2 -2.2h1a2.2 2.2 0 0 0 1.55 -.64l.7 -.7a2.2 2.2 0 0 1 3.12 0l.7 .7a2.2 2.2 0 0 0 1.55 .64h1a2.2 2.2 0 0 1 2.2 2.2v1a2.2 2.2 0 0 0 .64 1.55l.7 .7a2.2 2.2 0 0 1 0 3.12l-.7 .7a2.2 2.2 0 0 0 -.64 1.55 v1a2.2 2.2 0 0 1 -2.2 2.2h-1a2.2 2.2 0 0 0 -1.55 .64l-.7 .7a2.2 2.2 0 0 1 -3.12 0l-.7 -.7a2.2 2.2 0 0 0 -1.55 -.64h-1a2.2 2.2 0 0 1 -2.2 -2.2v-1a2.2 2.2 0 0 0 -.64 -1.55l-.7 -.7a2.2 2.2 0 0 1 0 -3.12l.7 -.7a2.2 2.2 0 0 0 .64 -1.55 v-1" />
+                        </svg>
+                      </div>
+                      <div className="ml-6">
+                        <h3 className="mb-1 leading-5 text-gray-800 font-bold text-2xl">2,330</h3>
+                        <p className="text-gray-600 text-sm tracking-normal font-normal leading-5">Avg Sales</p>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="flex items-center pl-3 text-green-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-trending-up" width={24} height={24} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                          <path stroke="none" d="M0 0h24v24H0z" />
+                          <polyline points="3 17 9 11 13 15 21 7" />
+                          <polyline points="14 7 21 7 21 14" />
+                        </svg>
+                        <p className="text-green-400 text-xs tracking-wide font-bold leading-normal pl-1">7.2%</p>
+                      </div>
+                      <p className="font-normal text-xs text-right leading-4 text-green-400 tracking-normal">Increase</p>
+                    </div>
+                  </div>
+                  <div className="relative mb-3">
+                    <hr className="h-1 rounded-sm bg-gray-200" />
+                    <hr className="absolute top-0 h-1 w-7/12 rounded-sm bg-indigo-700" />
+                  </div>
+                  <h4 className="text-base text-gray-600 font-normal tracking-normal leading-5">Yearly Goal</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="pb-32 pt-16">
+          <div className="mx-auto">
+            <div className="flex flex-wrap flex-row-reverse items-center">
+              <div className="md:w-1/2 lg:w-2/3 w-full lg:pl-20 md:pl-10 sm:pl-0 pl-0">
+                <div className="py-2 text-color">
+                  <h1 className="text-2xl lg:text-6xl tracking-tighter text-black md:leading-snug f-f-l font-black">Easily manage sales through our invoicing system</h1>
+                  <h2 className="text-lg lg:text-3xl leading-7 text-black md:leading-10 f-f-r py-8">Here at Globex we take special care of what your organization needs instead of selling you a mass market tool that takes a one size fits all approach. I personally review each and every client business and oversee the team that tailores a solution</h2>
+                  <div className="flex items-center cursor-pointer pb-4 md:pb-0">
+                    <h3 className="f-f-r text-lg lg:text-2xl font-semibold underline text-indigo-700">Lets Get Started</h3>
+                    <div className="pl-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none">
+                        <path d="M13.1719 12L8.22192 7.04999L9.63592 5.63599L15.9999 12L9.63592 18.364L8.22192 16.95L13.1719 12Z" fill="#D53F8C" />
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="lg:w-1/3 md:w-1/2 w-full relative h-96 flex items-end justify-center">
+                <img className="absolute w-full h-full inset-0 object-cover object-center rounded-md" src="https://cdn.tuk.dev/assets/templates/prodify/invoicing-system.png" alt />
+                <div className="relative z-10 p-4 bg-white shadow rounded mx-auto w-9/12 -mb-20">
+                    <h4 className="text-gray-800 font-normal text-md leading-4 tracking-normal mb-2">Invoice # 35RD87</h4>
+                    <p className="text-gray-600 text-xs leading-3 font-normal">Assigned to: Josh Rollins</p>
+                    <div className="mt-4 flex items-center">
+                        <span className="text-gray-600">
+                            <svg className="feather feather-user" xmlns="http://www.w3.org/2000/svg" width={16} height={16} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+                                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
+                                <circle cx={12} cy={7} r={4} />
+                            </svg>
+                        </span>
+                        <p className="ml-1 text-sm text-gray-800 leading-3 tracking-normal font-normal">Specter Consultancy</p>
+                    </div>
+                    <div className="mt-3 flex items-center">
+                        <span className="text-gray-600">
+                            <svg className="feather feather-dollar-sign" xmlns="http://www.w3.org/2000/svg" width={16} height={16} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+                                <line x1={12} y1={1} x2={12} y2={23} />
+                                <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                            </svg>
+                        </span>
+                        <p className="ml-1 text-sm text-gray-800 leading-3 tracking-normal font-normal">2,354 USD</p>
+                    </div>
+                    <button className="relative focus:outline-none mt-4 py-2 pr-10 bg-indigo-700 text-white tracking-normal text-xs pl-3 cursor-pointer hover:opacity-90">
+                        View Invoice
+                        <svg xmlns="http://www.w3.org/2000/svg" className="absolute inset-0 m-auto mr-3 icon icon-tabler icon-tabler-arrow-right" width={16} height={16} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" />
+                            <line x1={5} y1={12} x2={19} y2={12} />
+                            <line x1={13} y1={18} x2={19} y2={12} />
+                            <line x1={13} y1={6} x2={19} y2={12} />
+                        </svg>
+                    </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-gray-50 py-20 flex flex-col items-center justify-center">
+            <div className="xl:w-1/2 w-11/12">
+                <h1 role="heading" tabIndex={0} className="text-6xl font-bold 2xl:leading-10 leading-0 text-center text-gray-800">
+                    Touching hundreds of lives
+                </h1>
+                <h2 role="contentinfo" tabIndex={0} className="text-base leading-normal text-center text-gray-600 mt-5">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text Lorem Ipsum is simply dummy text of the printing
+                </h2>
+            </div>
+            <div className="2xl:px-20 lg:px-12 px-4 flex flex-wrap items-start mt-4">
+                <div className="mt-24">
+                    <div className="flex items-end ">
+                        <img tabIndex={0} src="https://i.ibb.co/kBgtHK6/Rectangle-5.png" alt="girl with blue background" className="w-20 h-20 rounded-lg mr-6" />
+                        <img tabIndex={0} src="https://i.ibb.co/9nLBtjx/Rectangle-3.png" alt="guy winking" className="w-48 h-36 rounded-lg" />
+                    </div>
+                    <div className="flex items-center justify-end my-6">
+                        <img tabIndex={0} src="https://i.ibb.co/jRbF1KF/Rectangle-4.png" alt="guy smiling" />
+                    </div>
+                    <div className="flex items-start">
+                        <img tabIndex={0} src="https://i.ibb.co/Sf4Q94L/Rectangle-6.png" alt="girl with bluw background" className="w-48 h-48 rounded-lg" />
+                        <img tabIndex={0} src="https://i.ibb.co/fnNqJrx/Rectangle-7.png" alt="guy with glasses" className="w-20 h-20 rounded-lg ml-6 flex-shrink-0 object-cover object-fit" />
+                    </div>
+                </div>
+                <div className="ml-6 mt-32">
+                    <img tabIndex={0} src="https://i.ibb.co/LSxy7fy/Rectangle-9.png" className="w-72 h-80 rounded-lg" alt="guy with sunglasses" />
+                    <div className="flex items-start mt-6">
+                        <img tabIndex={0} src="https://i.ibb.co/X8PKD3q/Rectangle-8.png" alt="girl  laughing" className="w-48 h-48 rounded-lg" />
+                        <img tabIndex={0} src="https://i.ibb.co/2Yj51CY/Rectangle-13.png" alt="guy with glasses" className="w-20 h-20 rounded-lg ml-6 object-cover object-fit" />
+                    </div>
+                </div>
+                <div className="mt-14 ml-6">
+                    <div className="lg:flex ">
+                        <div>
+                            <img tabIndex={0} src="https://i.ibb.co/bWGVSkP/Rectangle-10.png" alt="group of friends" className="w-96 h-72 rounded-lg object-center object-fit" />
+                        </div>
+                        <div>
+                            <div className="flex ml-6">
+                                <img tabIndex={0} src="https://i.ibb.co/80jvpSv/Rectangle-16.png" className="w-20 h-20 rounded-lg mt-14" alt="man" />
+                                <img tabIndex={0} src="https://i.ibb.co/6PR2Y74/Rectangle-15.png" className="w-20 h-24 rounded-lg ml-6" alt="woman" />
+                            </div>
+                            <img tabIndex={0} src="https://i.ibb.co/M5rvjhk/Rectangle-14.png" alt="boy with blonde hair" className="ml-6 mt-6 w-48 h-32 rounded-lg" />
+                        </div>
+                    </div>
+                    <div className="mt-6 flex">
+                        <img tabIndex={0} className="w-48 h-48 rounded-lg" src="https://i.ibb.co/GPpMsbr/Rectangle-12.png" alt="young girl with red hair" />
+                        <img tabIndex={0} className="w-72 h-56 rounded-lg ml-6" src="https://i.ibb.co/VBcgkVL/Rectangle-11.png" alt="young girl with red hair" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
   )
 }
 export default About;
