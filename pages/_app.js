a6bee616c484a89b09c1883547b21d2acf184779
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2022-09-29 14:42:28 +0500

    created new component for footer inside components direcotry and imported via _app.js to load on every page.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Navbar from '../components/navbar'
+import Footer from '../components/footer'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
@@ -6,6 +7,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </>
   )
 }
